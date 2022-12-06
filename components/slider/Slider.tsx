2ea2ad1080ddc40a8dd9c1954936a3425f4ceb7c
Ha Nhat Dan <hanhatdan@Has-MacBook-Pro.local>
2022-12-06 23:37:12 +0700

    update slide bar

--- components/slider/Slider.tsx
+++ components/slider/Slider.tsx
@@ -1,69 +1,103 @@
-import { useRef, useState } from "react";
-import { Card, renderCard } from "../../pages/home-banner";
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import { Card, renderCard } from '../../pages/home-banner';
 
 interface ISlider {
   ItemCard: Card[];
+  numberDisplay: number;
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
+  dot?: boolean;
+  step?: number;
+  timeOutPlay?: number;
 }
 
-export const CustomSlider = ({ ItemCard }: ISlider) => {
+export const CustomSlider = ({
+  ItemCard,
+  numberDisplay,
+  iconLeft,
+  iconRight,
+  dot = true,
+  step = 1,
+  timeOutPlay = 3000,
+}: ISlider) => {
   const [index, setIndex] = useState(0);
+  const [widthCard, setWidthCard] = useState(0);
   const refAvatar = useRef<HTMLDivElement>(null);
+  const refCard = useRef<HTMLDivElement>(null);
 
   const getTranslate = () => {
     if (refAvatar?.current?.offsetWidth) {
+      if (
+        (ItemCard.length - numberDisplay) * refAvatar.current.offsetWidth <
+        index * refAvatar.current.offsetWidth
+      ) {
+        return (
+          -(ItemCard.length - numberDisplay) * refAvatar.current.offsetWidth
+        );
+      }
       return -index * refAvatar.current.offsetWidth;
     }
     return 0;
   };
 
-  const getSpace = () => {
-    if (refAvatar?.current?.offsetWidth && window?.innerWidth) {
-      return Math.floor(window.innerWidth / refAvatar.current.offsetWidth);
-    }
-    return 0;
-  };
+  useEffect(() => {
+    setWidthCard((refCard?.current?.offsetWidth ?? 0) / numberDisplay);
+  }, [refAvatar, refCard]);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (index < ItemCard.length - numberDisplay) {
+        setIndex((prev) => prev + step);
+        clearTimeout(timeout);
+      }
+    }, timeOutPlay);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [index]);
   return (
     <div
       className="hide-scrollbar"
       style={{
-        width: "100%",
-        overflowX: "hidden",
-        position: "relative",
+        width: '100vw',
+        overflowX: 'hidden',
+        position: 'relative',
       }}
+      ref={refCard}
     >
       <div
         onClick={() => {
-          if (index >= getSpace()) {
-            setIndex((prev) => prev - getSpace());
+          if (index > step - 1) {
+            setIndex((prev) => prev - step);
           }
         }}
         style={{
-          position: "absolute",
+          position: 'absolute',
           left: 0,
-          bottom: "50%",
-          transform: "translateY(50%)",
-          borderRadius: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "30px",
+          bottom: '50%',
+          transform: 'translateY(50%)',
+          borderRadius: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '100px',
           zIndex: 101,
-          cursor: index > 0 ? "pointer" : "default",
+          cursor: index > 0 ? 'pointer' : 'default',
           opacity: index > 0 ? 1 : 0.5,
-          color: "white",
+          color: 'red',
         }}
       >
-        {"<"}
+        {'<'}
       </div>
 
       <div
         style={{
           transform: `translateX(${getTranslate()}px)`,
-          display: "flex",
-          gap: 20,
+          display: 'flex',
+          // gap: 20,
           marginBottom: 10,
-          height: "100%",
+          height: '100%',
+          width: 'min-content',
         }}
       >
         {ItemCard.map((item) => {
@@ -72,11 +106,7 @@ export const CustomSlider = ({ ItemCard }: ISlider) => {
               ref={refAvatar}
               key={item.uuid}
               style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 5,
-                alignItems: "center",
-                justifyContent: "start",
+                width: `${widthCard}px`,
               }}
             >
               {renderCard(item)}
@@ -87,28 +117,67 @@ export const CustomSlider = ({ ItemCard }: ISlider) => {
 
       <div
         onClick={() => {
-          if (index < ItemCard.length - getSpace()) {
-            setIndex((prev) => prev + getSpace());
+          if (index < ItemCard.length - numberDisplay) {
+            setIndex((prev) => prev + step);
           }
         }}
         style={{
-          position: "absolute",
+          position: 'absolute',
           right: 0,
-          bottom: "50%",
-          transform: "translateY(50%)",
-          borderRadius: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "30px",
+          bottom: '50%',
+          transform: 'translateY(50%)',
+          borderRadius: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '100px',
           zIndex: 101,
-          cursor: index < ItemCard.length - 1 ? "pointer" : "default",
-          opacity: index < ItemCard.length - 2 ? 1 : 0.5,
-          color: "white",
+          cursor:
+            index < ItemCard.length - numberDisplay ? 'pointer' : 'default',
+          opacity: index < ItemCard.length - numberDisplay ? 1 : 0.5,
+          color: 'red',
         }}
       >
-        {">"}
+        {'>'}
       </div>
+
+      {dot ? (
+        <div
+          style={{
+            position: 'absolute',
+            right: '50%',
+            bottom: '100px',
+            transform: 'translateX(50%)',
+            borderRadius: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '100px',
+            zIndex: 101,
+            color: 'red',
+            gap: 10,
+          }}
+        >
+          {new Array(Math.ceil((ItemCard.length - numberDisplay) / step + 1))
+            .fill(null)
+            .map((item, index1) => {
+              return (
+                <div
+                  key={index1}
+                  style={{
+                    width: 20,
+                    height: 20,
+                    borderRadius: '100%',
+                    backgroundColor: 'red',
+                    opacity: index1 * step === index ? '100%' : '50%',
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => setIndex(index1 * step)}
+                ></div>
+              );
+            })}
+        </div>
+      ) : null}
     </div>
   );
 };
